0c679df06483b6978521ebbe6b1cbcb33657a867
ChorizoMagico <juanestebanarias.2006@gmail.com>
2026-02-23 23:37:53 -0500

    saving changes with valeria's functions

--- Taller1FLP.rkt
+++ Taller1FLP.rkt
@@ -1,5 +1,90 @@
 #lang eopl
 
+;;1
+(define invert (lambda (L P)
+                 (letrec (
+                          (aplicarPredicado (lambda (L P)
+                                             (if
+                                              (and (P (car L)) (P (cadr L))) (cons (cadr L) (cons (car L) '()))
+                                              '()
+                                              ))))
+                   (cond
+                     [(null? L) '()]
+                     [(null? (aplicarPredicado (car L) P)) (invert (cdr L) P)]
+                     [else (cons (aplicarPredicado (car L) P) (invert (cdr L) P))]
+                     ))))
+
+;;2
+(define down (lambda (L) 
+                        (if
+                          (null? L) '()
+                          (cons (cons (car L) '()) (down (cdr L)))
+                         )))
+
+;;3
+(define list-set (lambda (L n x P)
+                   (letrec (
+                            (acumulador (lambda (L n x P acum)
+                                          (if
+                                           (eq? acum n)
+                                           (if (P (car L))
+                                               (cons x (cdr L))
+                                               L)
+                                           (cons (car L) (acumulador (cdr L) n x P (+ 1 acum)))
+                                           ))))
+                     (cond
+                       [(null? L) '()]
+                       [else (acumulador L n x P 0)]
+                       ))))
+
+;;4
+(define filter-in (lambda (P L)
+                                (cond
+                                 [(null? L) '()]
+                                 [(P (car L)) (cons (car L) (filter-in P (cdr L)))]
+                                 [else (filter-in P (cdr L))]
+                                 )))
+
+
+;;5
+(define palindrome? (lambda (palabra)
+                      (letrec (
+                               (invertirPalabra (lambda (L acum)
+                                 (if
+                                  (null? L) acum
+                                  (invertirPalabra (cdr L) (cons (car L) acum))
+                                  ))))
+                        (if
+                         (null? palabra) '()
+                         (equal? palabra (invertirPalabra palabra '()))
+                         ))))
+
+;;6
+(define swapper (lambda (E1 E2 L)
+                                (cond
+                                 [(null? L) '()]
+                                 [(eq? (car L) E1) (cons E2 (swapper E1 E2 (cdr L)))]
+                                 [(eq? (car L) E2) (cons E1 (swapper E1 E2 (cdr L)))]
+                                 [else (cons (car L) (swapper E1 E2 (cdr L)))]
+                                 )))
+
+;;7
+(define cartesian-product (lambda (L1 L2)
+                      (letrec (
+                               (unirListas (lambda (L1 L2)
+                                 (if (null? L1) L2
+                                     (cons (car L1) (unirListas (cdr L1) L2)))))
+                               (combinacion (lambda (L1 L2)
+                                 (cond
+                                  [(null? L1) '()]
+                                  [(null? L2) '()]
+                                  [else (cons (list (car L1) (car L2)) (combinacion L1 (cdr L2)))]
+                                  ))))
+                        (cond
+                         [(null? L1) '()]
+                         [(null? L2) '()]
+                         [else (unirListas (combinacion L1 L2) (cartesian-product (cdr L1) L2))]
+                         ))))
 
 ;;8
 
@@ -20,9 +105,6 @@
                          (if (null? L2) L1 (unirListas (concatenarAlFinal L1 (car L2)) (cdr L2)))
                          )))
     (if (null? L1) '() (unirListas (mapearUno F (car L1) L2) (mapping F (cdr L1) L2))))))
-
-
-
 
 
 ;;9
@@ -76,6 +158,7 @@
                         [else (filter-acum (+ a 1) b F acum filter)]
                         )
                       ))
+
 ;;13
 
 
@@ -88,7 +171,6 @@
                                        )
                                      )))
                     (operarConAcum lrators (cdr lrands) (car lrands) ))))
-
 
 ;;14
 
@@ -137,6 +219,29 @@
                          ]))))
                    
                    
+
+
+;;16
+(define hanoi (lambda (n origen auxiliar destino)
+                (letrec (
+                         (unirListas (lambda (L1 L2)
+                                 (if (null? L1) L2
+                                     (cons (car L1) (unirListas (cdr L1) L2))))))
+                                                 (cond
+                                                  [(= n 0) '()]
+                                                  [(= n 1) (cons (list origen destino) '())]
+                                                  [else (unirListas (hanoi (- n 1) origen destino auxiliar) (unirListas (list (list origen destino)) (hanoi (- n 1) auxiliar origen destino)))]
+                                                  ))))
+
+;;17
+(define coin-change (lambda (monto monedas)
+                                          (cond
+                                            [(= monto 0) 1]
+                                            [(< monto 0) 0]
+                                            [(null? monedas) 0]
+                                            [else (+ (coin-change (- monto (car monedas)) monedas) (coin-change monto (cdr monedas)))]
+                                            )))
+
  ;;18
 (define pascal (lambda (N)
                  (letrec (
